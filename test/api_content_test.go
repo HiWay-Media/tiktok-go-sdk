42c2477c0108b5c78906d96e1b1399404baf60ef
Allan Nava <allannava95@gmail.com>
2024-07-28 14:14:28 +0200

    added TestGetVideoList

--- test/api_content_test.go
+++ test/api_content_test.go
@@ -11,7 +11,21 @@ func TestCreatorInfo(t *testing.T) {
 		t.Fatalf(err.Error())
 	}
 	c.IsDebug()
-	resp, err := c.CreatorInfo()
+	/*resp, err := c.CreatorInfo()
+	if err != nil {
+		t.Fatalf(err.Error())
+	}*/
+	log.Println("resp ", c.IsDebug())
+}
+
+
+func TestGetVideoList( t *testing.T){
+	c, err := GetTikTok()
+	if err != nil {
+		t.Fatalf(err.Error())
+	}
+	c.IsDebug()
+	resp, err := c.GetVideoList(int64(20))
 	if err != nil {
 		t.Fatalf(err.Error())
 	}
