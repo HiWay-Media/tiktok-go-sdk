34586f9fa2d72ed2f8729610e67a13b9459ae512
allan <allan.nava@ies-italia.it>
2024-07-28 10:50:04 +0200

    solve model user

--- tiktok/model_user.go
+++ tiktok/model_user.go
@@ -1,18 +1,16 @@
 package tiktok
 
-
 type UserInfoResponse struct {
-	Data  DataUserInfo  `json:"data"`
-	Error ErrorObject `json:"error"`
+	Data  DataUserInfo `json:"data"`
+	Error ErrorObject  `json:"error"`
 }
 
-
 type DataUserInfo struct {
-	User  DataUserInfo  `json:"user"`
+	User UserInfo `json:"user"`
 }
 
 type UserInfo struct {
-	OpenID          string   `json:"open_id"`
-	UnionID         string   `json:"union_id"`
-	AvatarUrl       string   `json:"avatar_url"`
+	OpenID    string `json:"open_id"`
+	UnionID   string `json:"union_id"`
+	AvatarUrl string `json:"avatar_url"`
 }
--- tiktok/user.go
+++ tiktok/user.go
@@ -1,18 +1,23 @@
 package tiktok
 
+import (
+	"encoding/json"
+	"fmt"
+)
+
 /*
 Get User Info
 
 The /v2/user/info/ endpoint returns some basic information for a given TikTok user.
 
-HTTP URL
+# HTTP URL
 
 Request:
 curl -L -X GET 'https://open.tiktokapis.com/v2/user/info/?fields=open_id,union_id,avatar_url' \
 -H 'Authorization: Bearer act.example12345Example12345Example'
 */
 func (o *tiktok) UserInfo() (*UserInfoResponse, error) {
-    resp, err := o.restyGet(API_USER_INFO, nil)
+	resp, err := o.restyGet(API_USER_INFO, nil)
 	if err != nil {
 		return nil, err
 	}
