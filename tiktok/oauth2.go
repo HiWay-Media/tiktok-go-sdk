b517af26a6f1d264fc01a7531fdce86fa7efb538
Allan Nava <allannava95@gmail.com>
2024-07-29 12:31:52 +0200

    solve path

--- tiktok/oauth2.go
+++ tiktok/oauth2.go
@@ -10,7 +10,7 @@ import (
 // Endpoint is TikTok's OAuth 2.0 endpoint.
 var Endpoint = oauth2.Endpoint{
 	AuthURL:  "https://www.tiktok.com/v2/auth/authorize/",
-	TokenURL: "https://open.tiktokapis.com/v2/oauth/token",
+	TokenURL: "https://open.tiktokapis.com/v2/oauth/token/",
 }
 
 /*
