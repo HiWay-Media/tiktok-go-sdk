68a1ba5b5a927a76696cbdc53d68560474560ee5
Allan Nava <allannava95@gmail.com>
2024-09-19 12:43:39 +0200

    solve Authorization

--- tiktok/resty.go
+++ tiktok/resty.go
@@ -28,7 +28,7 @@ func (o *tiktok) restyPost(url string, body interface{}) (*resty.Response, error
 	resp, err := o.restClient.R().
 		SetHeader("Accept", "application/json").
 		SetHeader("Content-Type", "application/json").
-		SetHeader("Auhtorization", "Bearer "+o.accessToken).
+		SetHeader("Authorization", "Bearer "+o.accessToken).
 		SetBody(body).
 		Post(url)
 
@@ -55,7 +55,7 @@ func (o *tiktok) restyPostWithQueryParams(url string, body interface{},  queryPa
 	resp, err := o.restClient.R().
 		SetHeader("Accept", "application/json").
 		SetHeader("Content-Type", "application/json").
-		SetHeader("Auhtorization", "Bearer "+o.accessToken).
+		SetHeader("Authorization", "Bearer "+o.accessToken).
 		SetQueryParams(queryParams).
 		SetBody(body).
 		Post(url)
@@ -68,7 +68,7 @@ func (o *tiktok) restyPostWithQueryParams(url string, body interface{},  queryPa
 
 func (o *tiktok) restyGet(url string, queryParams map[string]string) (*resty.Response, error) {
 	resp, err := o.restClient.R().
-		SetHeader("Auhtorization", "Bearer "+o.accessToken).
+		SetHeader("Authorization", "Bearer "+o.accessToken).
 		SetQueryParams(queryParams).
 		Get(url)
 	//
