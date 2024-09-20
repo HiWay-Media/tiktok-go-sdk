b593d8a4987ea1fbc4cc5613567a4c35683fad4e
Allan Nava <allannava95@gmail.com>
2024-09-20 09:20:52 +0200

    added FOLLOWER_OF_CREATOR of privacyLevelOptions

--- tiktok/privacy.go
+++ tiktok/privacy.go
@@ -7,15 +7,16 @@ package tiktok
 type PrivacyLevelOptions string
 
 const (
-    PUBLIC_TO_EVERYONE PrivacyLevelOptions = "PUBLIC_TO_EVERYONE"
-    MUTUAL_FOLLOW_FRIENDS PrivacyLevelOptions = "MUTUAL_FOLLOW_FRIENDS"
-    SELF_ONLY PrivacyLevelOptions = "SELF_ONLY"
+    PUBLIC_TO_EVERYONE PrivacyLevelOptions 		= "PUBLIC_TO_EVERYONE"
+    MUTUAL_FOLLOW_FRIENDS PrivacyLevelOptions 	= "MUTUAL_FOLLOW_FRIENDS"
+	FOLLOWER_OF_CREATOR PrivacyLevelOptions 	= "FOLLOWER_OF_CREATOR"
+    SELF_ONLY PrivacyLevelOptions 				= "SELF_ONLY"
 )
 
 // CheckPrivacyLevel checks if the given string matches any PrivacyLevelOptions
 func CheckPrivacyLevel(level string) bool {
 	switch PrivacyLevelOptions(level) {
-	case PUBLIC_TO_EVERYONE, MUTUAL_FOLLOW_FRIENDS, SELF_ONLY:
+	case PUBLIC_TO_EVERYONE, MUTUAL_FOLLOW_FRIENDS, FOLLOWER_OF_CREATOR, SELF_ONLY:
 		return true
 	default:
 		return false
